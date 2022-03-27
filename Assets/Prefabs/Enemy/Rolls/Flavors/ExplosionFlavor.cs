53020e63ed6c752f6a6f455fc9a009c12061dc02
sangyeong <b7squirrel@gmail.com>
2022-03-26 20:42:35 -0700

    폭발할 때 폭발모양과 같이 collider생성
    - 폭발은 어느정도 마무리 되었음
    -  패링작업을 해야함

--- Assets/Prefabs/Enemy/Rolls/Flavors/ExplosionFlavor.cs
+++ Assets/Prefabs/Enemy/Rolls/Flavors/ExplosionFlavor.cs
@@ -9,13 +9,12 @@ public class ExplosionFlavor : MonoBehaviour
     public GameObject[] explosionEffect = new GameObject[3];
 
     private BoxCollider2D boxCol_1;  // 1Â÷ Æø¹ß
-    private BoxCollider2D boxCol_2;  // 2Â÷ Æø¹ß
-    private BoxCollider2D boxCol_3;  // 3Â÷ Æø¹ß
+    private BoxCollider2D[] boxCol = new BoxCollider2D[8];  
+
+    private Vector2 centerOffset_1;
+    private Vector2 centerOffset_2;
 
     private Vector2 centerOffset_0;
-    private Vector2 centerOffset_1;
-
-    private Vector2 centerCore;
     private Vector2[,] centerOutside = new Vector2[2, 4];
 
     private Vector2 boxSizeCore;
@@ -45,15 +44,23 @@ public class ExplosionFlavor : MonoBehaviour
     public int numberOfFlavors;
 
     private Vector2[] explosionSize = new Vector2[3];
+    private bool temp;
 
 
     private void Start()
     {
+        
+
+
+        boxCol_1 = GetComponent<BoxCollider2D>();
         isLastExplosion = false;
         InitiateExplosionMatrix(numberOfFlavors);
-        Instantiate(explosionEffect[0], centerCore, Quaternion.identity);
-        Explode(centerCore, boxSizeCore, 0);
-       
+        Instantiate(explosionEffect[0], centerOffset_0, Quaternion.identity);
+        Explode(centerOffset_0, boxSizeCore, 0);
+        //½Ã°£ ¸ØÃß°í Ä«¸Þ¶ó½¦ÀÌÅ©
+        GameManager.instance.StartCameraShake(8, .9f);
+        //GameManager.instance.TimeStop(.1f);
+
     }
 
     private void Update()
@@ -74,13 +81,16 @@ public class ExplosionFlavor : MonoBehaviour
             {
                 DelayExplosion(1, 2);
                 isLastExplosion = false;
+                GenerateColliders();
+
+                
                 Destroy(gameObject);
             }
         }
+
     }
     void DelayExplosion(int _indexOfSize, int _explosionIndex)
     {
-        
         for (int indexOfCenter = 0; indexOfCenter < 4; indexOfCenter++)
         {
             Instantiate(explosionEffect[_explosionIndex], centerOutside[_indexOfSize, indexOfCenter], Quaternion.identity);
@@ -107,6 +117,25 @@ public class ExplosionFlavor : MonoBehaviour
                 }
             }
         }
+        
+
+        boxCol_1.offset = Vector2.zero;
+        boxCol_1.size = boxSizeCore;
+        boxCol_1.isTrigger = true;
+    }
+
+    // offsetÀÌ¹Ç·Î tranform.position ¸¸Å­ »©Áà¾ß ÇÑ´Ù
+    private void GenerateColliders()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                boxCol[j] = gameObject.AddComponent<BoxCollider2D>();
+                boxCol[j].offset = centerOutside[i, j] - centerOffset_0;
+                boxCol[j].size = boxSize[i];
+            }
+        }
     }
 
     private void GenerateDebris(Vector2 _expPoint, Vector2 _DebrisPoint)
@@ -116,7 +145,7 @@ public class ExplosionFlavor : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0, dotAlpha);
-        Gizmos.DrawCube(centerCore, boxSizeCore);
+        Gizmos.DrawCube(centerOffset_0, boxSizeCore);
 
         Gizmos.color = new Color(0, 1, 0, dotAlpha);
         Gizmos.DrawCube(centerOutside[0, 0], boxSize[0]);
@@ -136,19 +165,19 @@ public class ExplosionFlavor : MonoBehaviour
         boxSize[0] = boxSizeCore * scale_2;
         boxSize[1] = boxSize[0] * scale_3;
 
-        centerOffset_0 = boxSizeCore * .5f + boxSize[0] * .5f;
-        centerOffset_1 = boxSize[0] * .5f + boxSize[1] * .5f;
+        centerOffset_1 = boxSizeCore * .5f + boxSize[0] * .5f;
+        centerOffset_2 = boxSize[0] * .5f + boxSize[1] * .5f;
 
-        centerCore = transform.position;
+        centerOffset_0 = transform.position;
 
-        centerOutside[0, 0] = centerCore + new Vector2(centerOffset_0.x, 0);
-        centerOutside[0, 1] = centerCore + new Vector2(-centerOffset_0.x, 0);
-        centerOutside[0, 2] = centerCore + new Vector2(0, centerOffset_0.y);
-        centerOutside[0, 3] = centerCore + new Vector2(0, -centerOffset_0.y);
+        centerOutside[0, 0] = centerOffset_0 + new Vector2(centerOffset_1.x, 0);
+        centerOutside[0, 1] = centerOffset_0 + new Vector2(-centerOffset_1.x, 0);
+        centerOutside[0, 2] = centerOffset_0 + new Vector2(0, centerOffset_1.y);
+        centerOutside[0, 3] = centerOffset_0 + new Vector2(0, -centerOffset_1.y);
 
-        centerOutside[1, 0] = centerOutside[0, 0] + new Vector2(centerOffset_1.x, 0);
-        centerOutside[1, 1] = centerOutside[0, 1] + new Vector2(-centerOffset_1.x, 0);
-        centerOutside[1, 2] = centerOutside[0, 2] + new Vector2(0, centerOffset_1.y);
-        centerOutside[1, 3] = centerOutside[0, 3] + new Vector2(0, -centerOffset_1.y);
+        centerOutside[1, 0] = centerOutside[0, 0] + new Vector2(centerOffset_2.x, 0);
+        centerOutside[1, 1] = centerOutside[0, 1] + new Vector2(-centerOffset_2.x, 0);
+        centerOutside[1, 2] = centerOutside[0, 2] + new Vector2(0, centerOffset_2.y);
+        centerOutside[1, 3] = centerOutside[0, 3] + new Vector2(0, -centerOffset_2.y);
     }
 }
